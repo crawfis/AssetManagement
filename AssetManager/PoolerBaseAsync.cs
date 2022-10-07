72b68be85dd30d7a5f2d8ba6de5e1f16b38a543c
Roger Crawfis <crawfis.3@osu.edu>
2022-10-07 12:18:57 -0400

    Removed Unity's Object pooling and wrote my own that is async.

--- AssetManager/PoolerBaseAsync.cs
+++ AssetManager/PoolerBaseAsync.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using UnityEngine.Pool;
 
 namespace CrawfisSofware.AssetManagement
 {
@@ -10,73 +9,72 @@ namespace CrawfisSofware.AssetManagement
     /// <typeparam name="T">The type of instances in the pool.</typeparam>
     public abstract class PoolerBaseAsync<T> : IAssetManagerAsync<T> where T : class
     {
-        protected ObjectPool<T> _pool;
-        protected string _currentAssetName;
+        private readonly Dictionary<string, Queue<T>> _pools = new Dictionary<string, Queue<T>>();
+        private readonly Dictionary<T,string> _allocatedAssets = new Dictionary<T,string>();
 
-        public Task<T> GetAsync(string name)
+        public async Task<T> GetAsync(string name)
         {
-            _currentAssetName = name;
-            //T asset = await Task<T>.Run(() => { T result = _pool.Get(); return result; });
-            T asset = _pool.Get();
-            return Task.FromResult(asset);
+            T instance;
+            if (!_pools.TryGetValue(name, out Queue<T> pool))
+            {
+                pool = new Queue<T>();
+                _pools[name] = pool;
+            }
+            if (pool.Count == 0)
+            {
+                instance = await CreateNewPoolInstanceAsync(name);
+            }
+            else
+            {
+                instance = pool.Dequeue();
+            }
+            _allocatedAssets.Add(instance, name);
+            ReinitializePoolInstance(instance);
+            return instance;
         }
+
         public Task ReleaseAsync(T poolObject)
         {
-            _pool.Release(poolObject);
+            if (_allocatedAssets.TryGetValue(poolObject, out string poolName))
+            {
+                _pools[poolName].Enqueue(poolObject);
+                _allocatedAssets.Remove(poolObject);
+                ReturnPoolInstance(poolObject);
+            }
             return Task.CompletedTask;
         }
 
         public Task ReleaseAllAsync()
         {
-            _pool.Clear();
+            foreach (var pool in _pools.Values)
+            {
+                while (pool.Count > 0)
+                {
+                    T poolObject = pool.Dequeue();
+                    ReturnPoolInstance(poolObject);
+                }
+                pool.Clear();
+            }
+            _pools.Clear();
+            _allocatedAssets.Clear();
             return Task.CompletedTask;
         }
 
         public abstract IEnumerable<string> AvailableAssets();
 
-        public PoolerBaseAsync(int initialSize = 100, int maxPersistentSize = 10000, bool collectionChecks = false)
+        public PoolerBaseAsync()
         {
-            InitPool(initialSize, maxPersistentSize, collectionChecks);
+            //InitPool(initialSize, maxPersistentSize, collectionChecks);
         }
 
-        protected void InitPool(int initial = 10, int maxPersistentSize = 20, bool collectionChecks = false)
-        {
-            _pool = new ObjectPool<T>(
-                CreateNewPoolInstance,
-                GetPoolInstance,
-                ReleasePoolInstance,
-                DestroyPoolInstance,
-                collectionChecks,
-                initial,
-                maxPersistentSize);
-        }
+        //protected void InitPool(int initial = 10, int maxPersistentSize = 20, bool collectionChecks = false)
+        //{
+        //}
 
         protected abstract Task<T> CreateNewPoolInstanceAsync(string name);
         protected abstract void ReinitializePoolInstance(T poolObject);
         protected abstract void ReturnPoolInstance(T poolObject);
         protected abstract Task DestroyPoolInstanceAsync(T poolObject);
         public abstract Task Initialize();
-
-        private T CreateNewPoolInstance()
-        {
-            Task<T> task = CreateNewPoolInstanceAsync(_currentAssetName);
-            task.GetAwaiter().GetResult();
-            return task.Result;
-        }
-
-        private void GetPoolInstance(T poolObject)
-        {
-            ReinitializePoolInstance(poolObject);
-        }
-
-        private void ReleasePoolInstance(T poolObject)
-        {
-            ReturnPoolInstance(poolObject);
-        }
-        private void DestroyPoolInstance(T poolObject)
-        {
-            Task task = DestroyPoolInstanceAsync(poolObject);
-            task.Wait();
-        }
     }
 }
